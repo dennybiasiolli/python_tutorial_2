901d6701ded344ccf2942a3db0101b08eefc500f
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-05-06 12:55:34 +0200

    string_formatting added

---
+++ string_formatting.py
@@ -0,0 +1,46 @@
+nums = [4, 5, 6]
+msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
+print(msg)
+
+a = "{x}, {y}".format(x=5, y=12)
+print(a)
+
+print(", ".join(["spam", "eggs", "ham"]))
+# prints "spam, eggs, ham"
+
+print("Hello ME".replace("ME", "world"))
+# prints "Hello world"
+
+print("This is a sentence.".startswith("This"))
+# prints "True"
+
+print("This is a sentence.".endswith("sentence."))
+# prints "True"
+
+print("This is a sentence.".upper())
+# prints "THIS IS A SENTENCE."
+
+print("AN ALL CAPS SENTENCE".lower())
+# prints "an all caps sentence"
+
+print("spam, eggs, ham".split(", "))
+# prints "['spam', 'eggs', 'ham']"
+
+
+print(min(1, 2, 3, 4, 0, 2, 1))
+print(max([1, 4, 9, 2, 5, 6, 8]))
+print(abs(-99))
+print(abs(42))
+print(sum([1, 2, 3, 4, 5]))
+
+
+nums = [55, 44, 33, 22, 11]
+
+if all([i > 5 for i in nums]):
+    print("All larger than 5")
+
+if any([i % 2 == 0 for i in nums]):
+    print("At least one is even")
+
+for v in enumerate(nums):
+    print(v)
